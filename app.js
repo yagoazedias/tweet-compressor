43327e184364587c023c2c77e8edddc948b4cc65
Yago Luis Azedias de Melo <yagoazedias@gmail.com>
2017-01-06 03:28:20 -0200

    Added made-with-love

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('view engine', 'ejs');
 app.set('port', (process.env.PORT || 3000))
 
 app.use(express.static(__dirname + '/public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.listen(app.get('port'), function(){
   console.log("Server's running localy on port 5000.");
